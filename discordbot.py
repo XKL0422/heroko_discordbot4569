a3ecbae3724c25a979de927d4a84a45e9a0c549a
XKL0422 <109857936+XKL0422@users.noreply.github.com>
2022-07-24 14:39:27 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,8 +4,6 @@ import os
 from pprint import pprint
 # 輸入自己Bot的TOKEN碼
 TOKEN = os.environ['TOKEN']
-SRCLanguage=os.environ['SRC']
-DSTLanguage=os.environ['DST']
 
 client = discord.Client()
 
@@ -14,25 +12,19 @@ client = discord.Client()
 async def on_ready():
     print('成功登入') 
     
-# 收到訊息時呼叫
+#導入Discord.py
+import discord
+import time
+#client是我們與Discord連結的橋樑
+client = discord.Client()
+
 @client.event
+#當有訊息時
 async def on_message(message):
-    # 送信者為Bot時無視
-    if message.author.bot:
-        return
-    
-    if client.user in message.mentions: # @判定
-        translator = googletrans.Translator()
-        robotName = client.user.name
-        first, space, content = message.clean_content.partition('@'+robotName+' ')
+    if message.content == '我好帥喔':
+        #然後回傳訊息
+        await message.channel.send('不好意思，不要騙人啦')
         
-        if content == '':
-            content = first
-        if translator.detect(content).lang == DSTLanguage:
-            return
-        if translator.detect(content).lang == SRCLanguage or SRCLanguage == '':
-            remessage = translator.translate(content, dest='zh-tw').text
-            await message.reply(remessage) 
 
 # Bot起動
 client.run(TOKEN)
