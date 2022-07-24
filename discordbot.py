936acea4334113114e748e08ad0de873233158eb
XKL0422 <109857936+XKL0422@users.noreply.github.com>
2022-07-24 14:20:37 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,13 +14,32 @@ client = discord.Client()
 async def on_ready():
     print('成功登入') 
     
+# 收到訊息時呼叫
 @client.event
-#當有訊息時
 async def on_message(message):
-    if message.content == '碰6要碰啥':
-        #刪除傳送者的訊息
-        await message.delete()
-        #然後回傳訊息
-        await message.channel.send('肯定卡雷')
+    # 送信者為Bot時無視
+    if message.author.bot:
+        return
+    
+    if client.user in message.mentions: # @判定
+        translator = googletrans.Translator()
+        robotName = client.user.name
+        first, space, content = message.clean_content.partition('@'+robotName+' ')
         
+        if content == '':
+            content = first
+        if translator.detect(content).lang == DSTLanguage:
+            return
+        if translator.detect(content).lang == SRCLanguage or SRCLanguage == '':
+            remessage = translator.translate(content, dest='zh-tw').text
+            await message.reply(remessage) 
+
+# Bot起動
+client.run(TOKEN)
+#導入Discord.py
+import discord
+import time
+#client是我們與Discord連結的橋樑
+client = discord.Client()
+
 
