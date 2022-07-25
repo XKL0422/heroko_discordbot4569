c696d404a72383506c2b7bb4ad0b8c6769975182
XKL0422 <109857936+XKL0422@users.noreply.github.com>
2022-07-25 22:06:12 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,7 @@
 import discord
 import googletrans
 import os
+import random
 from pprint import pprint
 # 輸入自己Bot的TOKEN碼
 TOKEN = os.environ['TOKEN']
@@ -18,17 +19,59 @@ import time
 #client是我們與Discord連結的橋樑
 client = discord.Client()
         
+# 收到訊息時呼叫
 @client.event
-#當有訊息時
-import random
 async def on_message(message):
-    if message.content == '111':
-               def selectRandom(names):
-               return random.choice(names)
-               await message.channel.send = ['John', 'Juan', 'Jane', 'Jack', 'Jill', 'Jean']
-
-
-print("The name selected is: ", selectRandom(names))
+    
+    # 送信者為Bot時無視
+    if message.author.bot:
+        return
+    await client.process_commands(message)
+    #私訊
+    if message.guild == None:
+        return
+    result = service.spreadsheets().values().get(
+    spreadsheetId=SpreadsheetId, range=rangeName).execute()
+    values = result.get('values', [])
+    if not values:
+        return
+    else:
+        for row in values:
+            if (message.channel.name == row[0] or row[0] == ''):
+                keywords = row[1].split()
+                check = True
+                for keyword in keywords:
+                    if not keyword in message.content:
+                        check = False
+                        break
+                if check:
+                    if message.author.nick == None:
+                        username = message.author.name
+                    else:
+                            username = message.author.nick
+                    if '<ban>' in row[2]:
+                        await message.author.ban()
+                    else:
+                        if '<kick>' in row[2]:
+                            await message.author.kick()
+                        if '<delete>' in row[2]:
+                            await message.delete()
+                        else:
+                            if '<reply>' in row[2]:
+                                await message.reply(row[3].replace('<username>',username))
+                            if '<replyrandom>' in row[2]:
+                                msgs = row[3].split('|')
+                                if len(msgs) != 0:
+                                    index = random.randint(0, len(msgs)-1)
+                                    await message.reply(msgs[index].replace('<username>',username))
+                    if '<send>' in row[2]:
+                        await message.channel.send(row[3].replace('<username>',username))
+                    if '<sendrandom>' in row[2]:
+                        msgs = row[3].split('|')
+                        if len(msgs) != 0:
+                            index = random.randint(0, len(msgs)-1)
+                            await message.channel.send(msgs[index].replace('<username>',username))
+                    return
                 
 # Bot起動
 client.run(TOKEN)
